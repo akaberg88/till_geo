dc162721617759fd53dfed3b6b830211e6212cf7
akaberg88 <annika.aberg@hotmail.fi>
2016-10-18 12:57:48 +0000

    raw style for finding subtill sediment

--- sedi2.py
+++ sedi2.py
@@ -2,7 +2,10 @@
 """
 Created on Tue Oct 18 10:00:05 2016
 
-@author: root
+@author: Annika Åberg
+
+This code finds a till unit from targeting till geochemistry data 
+that is overlain by sub-till sorted sediment unit.
 """
 import glob
 import numpy as np
@@ -11,7 +14,7 @@ import numpy as np
 source_dir = "/home/geo/"
 file_paths = glob.glob("/home/geo/till_geo_num.csv")
 
-
+#Defining output file
 sedi_out = "/home/geo/sedi_out.csv"
 
 datas=np.loadtxt(fname ='till_geo_num.csv', delimiter = ',', skiprows = 1, usecols=(1,2,3,4,5))
@@ -23,8 +26,8 @@ ids = np.array(datas[:,0])
 x = np.array(datas[:,3])
 y = np.array(datas[:,4])
 
-joo = 'sub-till sorted'
-ei = 'empty'
+joo = 'covers sub-till sorted layer'
+ei = ''
 #print(sedim)
 #print(ids)
 #print(x)
@@ -32,17 +35,15 @@ ei = 'empty'
 for i in range(len(sedim)):
     sub_cond = ((x[i] == x[i+1]) & (y[i] == y[i+1]) & (sedim[i] == 0)& (sedim[i+1] == 1))
     if sub_cond == True:
-        line_joo = str(ids[i])+','+str(x[i])+',' +str(sedim[i])+','+joo
-        linejoo = str(joo)
+        line_joo = str(ids[i])+','+str(x[i])+','+ str(y[i])+','+str(sedim[i])+','+joo
         print(line_joo)
         with open (sedi_out,'a') as m:
-            m.write(linejoo+'\n')
+            m.write(line_joo+'\n')
     else:
-        line_ei = str(ids[i])+','+str(x[i]) +','+str(sedim[i])+','+ei
-        lineei= str(ei)
+        line_ei = str(ids[i])+','+str(x[i]) +','+str(y[i])+','+str(sedim[i])+','+ei
         print(line_ei)
         with open (sedi_out,'a') as m:
-            m.write(lineei+'\n')
+            m.write(line_ei+'\n')
         
 
 
