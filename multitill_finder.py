f9aca9f6de546409ac7a896f7d707a930665de20
akaberg88 <annika.aberg@hotmail.fi>
2017-01-03 13:28:03 +0000

    Final and fixed version of multi till finder (3.1.2017)

--- multitill_finder.py
+++ multitill_finder.py
@@ -4,21 +4,26 @@ Created on Tue Oct 18 10:00:05 2016
 
 @author: Annika Åberg
 
+This code finds drillings with multible tills from targeting till geochemisrty samples
+if the till units are seperated with a sorted sediment sample.
 Sediment codes:
     till, weathered bedrock till = 0
     sand, gravel, silt, clay = 1
     weathered bedrock = 2
     unknown = 3
 """
+import os
+os.getcwd()
+os.chdir('C:\\HY-Data\AKABERG\documents\Python')
 import glob
 import numpy as np
 
 #Defining the file path and source files
-source_dir = "/home/geo/"
-file_paths = glob.glob("/home/geo/till_geo_num.csv")
+source_dir = "C:\HY-Data\AKABERG\documents\Python"
+file_paths = glob.glob("C:\HY-Data\AKABERG\documents\Python\till_geo_num.csv")
 
 #Defining output file
-sedi_out = "/home/geo/multi_out.csv"
+sedi_out = "C:\HY-Data\AKABERG\documents\Python\multible_till_out.csv"
 
 datas=np.loadtxt(fname ='till_geo_num.csv', delimiter = ',', skiprows = 1, usecols=(1,2,3,4,5))
 
@@ -33,8 +38,26 @@ joo = 'Multible till'
 ei = 'No'
 
 for i in range(len(sedim)-1):
-    sub_cond = ((x[i] == x[i+2]) & (y[i]== y[i+2]) & (sedim[i] == 0) & (sedim[i+1] ==1) & (sedim[i+2] == 0))   
-    if sub_cond == True:        
+    sub_cond = ((x[i] == x[i-1] == x[i-2]) & (y[i] == y[i-1] == y[i-2]) & (sedim[i] == 0) & (sedim[i-1] == 1) & (sedim[i-2] == 0))
+    sub_cond_2 = ((x[i] == x[i-1] == x[i-2] == x[i-3]) & (y[i] == y[i-1] == y[i-2] == y[i-3]) & (sedim[i] == 0) & (sedim[i-1] == 1) & (sedim[i-2] == 1) & (sedim[i-3] == 0))
+    sub_cond_3 = ((x[i] == x[i-1] == x[i-2] == x[i-3] == x[i-4]) & (y[i] == y[i-1] == y[i-2]== y[i-3] == y[i-4]) & (sedim[i] == 0) & (sedim[i-1] == 1) & (sedim[i-2] == 1) &(sedim[i-3] == 1) & (sedim[i-4] == 0))
+    sub_cond_4 = ((x[i] == x[i-1] == x[i-2] == x[i-3] == x[i-4] == x[i-5]) & (y[i] == y[i-1] == y[i-2]== y[i-3] == y[i-4] == x[i-5]) & (sedim[i] == 0) & (sedim[i-1] == 1) & (sedim[i-2] == 1)&(sedim[i-3] == 1) & (sedim[i-4] == 1) & (sedim[i-5] == 0))
+    if sub_cond == True and i >= 2:        
+        line_joo = str(ids[i])+','+str(depth[i])+','+str(x[i])+','+ str(y[i])+','+str(sedim[i])+','+joo
+        print(line_joo)
+        with open (sedi_out,'a') as m:
+            m.write(line_joo+'\n')
+    elif sub_cond_2 == True and i >= 3:
+        line_joo = str(ids[i])+','+str(depth[i])+','+str(x[i])+','+ str(y[i])+','+str(sedim[i])+','+joo
+        print(line_joo)
+        with open (sedi_out,'a') as m:
+            m.write(line_joo+'\n')
+    elif sub_cond_3 == True and i >= 4:
+        line_joo = str(ids[i])+','+str(depth[i])+','+str(x[i])+','+ str(y[i])+','+str(sedim[i])+','+joo
+        print(line_joo)
+        with open (sedi_out,'a') as m:
+            m.write(line_joo+'\n')
+    elif sub_cond_4 == True and i >= 5:
         line_joo = str(ids[i])+','+str(depth[i])+','+str(x[i])+','+ str(y[i])+','+str(sedim[i])+','+joo
         print(line_joo)
         with open (sedi_out,'a') as m:
